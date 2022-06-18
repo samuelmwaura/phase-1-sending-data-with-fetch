f86792610b830810c265337ec5488480a0afe51a
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-18 08:47:11 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+// Add your code here();
+const  submitData =(name, email)=>{
+return fetch('http://localhost:3000/users',{
+method:"POST",
+headers:{
+    'content-type':'application/json',
+    Accept:'application/json'
+},
+body:JSON.stringify({
+    name:name,
+    email,email
+})
+}).then(response=>response.json())
+.then(data=>{
+    document.querySelector('body').innerHTML = `<p>${data.id}</p>`;
+})
+.catch(error=>{
+    document.querySelector('body').innerHTML =  `<p>${error.message}</p>`;
+}); 
+}
